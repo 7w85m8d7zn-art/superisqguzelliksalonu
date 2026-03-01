5d36c9d5662c6001c2867f74b031e5e7d0189a3d
Miraç Topuz <miractopuz@Yusuf-MacBook-Air.local>
2026-03-01 04:06:28 +0300

    update2

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
-  const res = NextResponse.redirect(new URL('/login', req.url))
+  const res = NextResponse.redirect(new URL('/', req.url))
   // delete cookie by setting expires in past
   res.cookies.set({ name: 'admin-auth', value: '', path: '/', expires: new Date(0) })
   return res
